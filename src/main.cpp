1f0c5fccc0fb8a3b090dfe7f898836ca3005e441
Ben Baker <bakerb845@gmail.com>
2026-09-21 14:08:51 -0600

    pure config error

--- src/main.cpp
+++ src/main.cpp
@@ -500,8 +500,6 @@ int main(int argc, char* argv[])
         std::string ldapOrganizationUnit{std::getenv("LDAP_ORGANIZATION_UNIT")};
         std::string ldapDomainComponent{std::getenv("LDAP_DOMAIN_COMPONENT")};
 */
-std::cout << programOptions.ldapOrganizationalUnit << std::endl;
-std::cout << programOptions.ldapDomainComponent << std::endl;
         constexpr bool maintainConnection{false};
         ldapAuthenticator
             = std::make_shared<CCTService::LDAP> 
